ae57b53490d355e151b0cffb37702832a60e602a
ArjunGTX <arjunatpavilion@gmail.com>
2022-03-13 14:05:23 +0530

    Initial Setup

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,27 +1,26 @@
 import { v4 as uuid } from "uuid";
 
-/**
- * Category Database can be added here.
- * You can add category of your wish with different attributes
- * */
-
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    categoryName: "Racing",
+    description: "Performance oriented, extremely aerodynamic helmets.",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "Off Road",
     description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+      "Specially designed helmets to provide confidence on tough terrains and off-road trials.",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "Street",
+    description: "Glossy and graphics printed helmets for a street show.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Touring",
     description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+      "Lightweight and comfortable helmets for long rides and highway cruising.",
   },
 ];
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 
-// Call make Server
 makeServer();
 
 ReactDOM.render(
