ab355bc4c38eb0598161989df7a3c9860558161b
ArjunGTX <arjunatpavilion@gmail.com>
2022-03-13 20:48:08 +0530

    WIP: added components, setup basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"/>
+      </Routes>
+    </BrowserRouter>
+  );
+
 };
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer class="fc-ct-ct py-xl pos-abs full-width">
+      <h5 className="txt-md font-medium">Made with Hands by Arjun</h5>
+      <p>Copyright @ ArjunDev | 2022</p>
+    </footer>
+  );
+};
