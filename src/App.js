5a005638d1c37496bfbf766dba3d0d14d6e21775
ArjunGTX <arjunatpavilion@gmail.com>
2022-03-16 16:32:52 +0530

    FEAT: auth pages UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PageContainer } from "./components";
-import { Home } from "./pages";
+import { ForgotPassword, Home, Login, ResetPassword, SignUp } from "./pages";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PageContainer page={<Home />} />} />
+        <Route path="/login" element={<PageContainer page={<Login />} />} />
+        <Route path="/sign-up" element={<PageContainer page={<SignUp />} />} />
+        <Route
+          path="/forgot-password"
+          element={<PageContainer page={<ForgotPassword />} />}
+        />
+        <Route
+          path="/reset-password"
+          element={<PageContainer page={<ResetPassword />} />}
+        />
       </Routes>
     </BrowserRouter>
   );
