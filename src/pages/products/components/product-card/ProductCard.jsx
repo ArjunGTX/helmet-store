d85da4e232b6c0f02da8aec29efa1341725f55fa
ArjunGTX <arjunatpavilion@gmail.com>
2022-03-17 21:21:59 +0530

    FIX: UI bugs, Rating Count

--- src/pages/products/components/product-card/ProductCard.jsx
+++ src/pages/products/components/product-card/ProductCard.jsx
@@ -22,7 +22,7 @@ export const ProductCard = ({ product }) => {
         <span className="price-old">{`₹${product.price}`}</span>
         <span className="offer">{`(${product.offer}% OFF)`}</span>
       </div>
-      <ProductRating rating={product.rating} />
+      <ProductRating rating={product.rating} ratingCount={product.ratingCount} />
       <div className="card-bottom">
         <button className="btn btn-primary">Add to Cart</button>
       </div>
--- src/pages/products/components/product-card/ProductRating.jsx
+++ src/pages/products/components/product-card/ProductRating.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
-export const ProductRating = ({ rating }) => {
+export const ProductRating = ({ rating, ratingCount }) => {
   return (
-    <div className="rating">
+    <div className="rating flex-row align-center">
       {[1,2,3,4,5].map((num) => num <= rating ? <AiFillStar key={num} className="filled"/> : <AiOutlineStar key={num} />)}
+      ({ratingCount})
     </div>
   );
 };
