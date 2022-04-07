2357ae4db564ff77f135d8eae2109255849ec561
ArjunGTX <arjunatpavilion@gmail.com>
2022-04-07 23:13:25 +0530

    removed useEffect

--- src/contexts/FilterContext.js
+++ src/contexts/FilterContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useContext, useEffect } from "react";
+import { createContext, useReducer, useContext } from "react";
 import { filterReducer, initialFilters } from "../reducers";
 
 const FilterContext = createContext(null);
@@ -8,7 +8,6 @@ export const useFilters = () => useContext(FilterContext);
 export const FilterProvider = ({ children }) => {
   const [filters, filterDispatch] = useReducer(filterReducer, initialFilters);
 
-  useEffect(() => console.log(filters.search), [filters.search]);
   return (
     <FilterContext.Provider value={{ filters, filterDispatch }}>
       {children}
